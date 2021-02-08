a3a4d44a25780ef9a249d95f6589ca281e51ca25
mericogold <mericogold@merico.gonze.com>
2021-02-08 15:48:10 -0800

    First and largest commit by mericogold

---
+++ README.md
@@ -0,0 +1,1 @@
+# top-contributor-meets-min-contributors
---
+++ app.js
@@ -0,0 +1,17 @@
+function helloWorld () {
+  console.log('Hello world')
+}
+
+function goodbyeWorld () {
+  console.log('Goodbye world')
+}
+
+const welcomeWorld = true
+
+if (welcomeWorld) {
+  helloWorld()
+} else {
+  goodbyeWorld()
+}
+
+console.log('Now this is done')
